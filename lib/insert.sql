ff029ad09bc068dfc78777e778473b25c780d8c9
Chevy <vallchevy@gmail.com>
2024-06-15 13:46:14 -0300

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ('Mr. Chocolate', 8, 'M', 'brown', 'friendly', true),
+    ('Mr. Chocolate', 8, 'M', 'brown', 'friendly', true),
+    ('Mr. Chocolate', 8, 'M', 'brown', 'friendly', true),
+    ('Mr. Chocolate', 8, 'M', 'brown', 'friendly', true),
+    ('Mr. Chocolate', 8, 'M', 'brown', 'friendly', true),
+    ('Mr. Chocolate', 8, 'M', 'brown', 'friendly', true),
+    ('Mr. Chocolate', 8, 'M', 'brown', 'friendly', true),
+    (NULL, 8, 'M', 'brown', 'friendly', true);
